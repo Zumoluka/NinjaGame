6e91fd1d1484d9b260c808cfafc3306c2db3fc89
Zumoluka <Nahuelcarrizo0809@gmail.com>
2024-06-19 00:43:33 -0300

    tp 4

--- Assets/Scripts/Coins and Boxes/ICollectable.cs
+++ Assets/Scripts/Coins and Boxes/ICollectable.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+namespace Items
+{
     public interface ICollectible
     {
         void Collect();
     }
-
+}
